a508e113142b87e0d2aae8d1669f94d50ce5bab4
Kieran Emery <kizwiz@hotmail.co.uk>
2022-11-23 19:09:56 +0000

    Make a new object of the ship called myCargoShip and then utilise the ToString() method to print out the ship details to the user.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,17 @@
-﻿internal class Program
+﻿using CargoShipLoader;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+
+        // Make a new copy/object of the ship.
+        Ship myCargoShip = new Ship();
+
+        // Uses ToString method to print out something about the ship details.
+        Console.WriteLine("This ship is " + myCargoShip.ToString());
+
         Console.ReadLine();
     }
 }
